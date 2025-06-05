d3bfc2948c5fbcee22067fa19fd9d1dd47a5b319
Juca Méndez <187219@upslp.edu.mx>
2025-06-04 23:00:50 -0600

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const hamburger = document.querySelector('.hamburger');
-    const navMenu = document.querySelector('.nav-links');
-    
-    hamburger.addEventListener('click', () => {
-        navMenu.classList.toggle('active');
-        hamburger.classList.toggle('active');
-    });
-});
